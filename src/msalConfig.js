56ec145b16137c526630f6142583c2c1fd4ecfae
Muthupandi <muthupandi1782000@gmail.com>
2024-11-06 21:22:29 +0530

    feat: build fix try

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -6,6 +6,10 @@ const msalConfig = {
     authority: `https://login.microsoftonline.com/${import.meta.env.VITE_API_TENANT_ID}`, // Replace with your Azure AD tenant ID
     redirectUri: window.location.origin,
   },
+  cache: {
+    cacheLocation: "sessionStorage", // You can change this to localStorage if needed
+    storeAuthStateInCookie: true, // Set to true for some browser compatibility
+  },
 };
 
 export const msalInstance = new PublicClientApplication(msalConfig);
